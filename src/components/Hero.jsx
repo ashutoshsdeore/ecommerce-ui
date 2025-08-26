7f0bdd03b0ad465e4277df8045a60eb731bf076f
ashutoshsdeore <deoreashutosh6@gmail.com>
2025-08-27 03:39:41 +0530

    Updated project code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 export default function Hero(){
   return (
     <section className="relative">
-      <img src="/assets/hero.svg" alt="Hero" className="w-full h-[46vh] md:h-[60vh] object-cover rounded-b-2xl"/>
+      <img src="/assets/hero.jpg" alt="Hero" className="w-full h-[46vh] md:h-[60vh] object-cover rounded-b-2xl"/>
       <div className="absolute inset-0 grid place-items-center text-center">
         <div className="text-white drop-shadow-lg p-4">
           <h1 className="text-3xl md:text-5xl font-bold">Style that sees you</h1>
